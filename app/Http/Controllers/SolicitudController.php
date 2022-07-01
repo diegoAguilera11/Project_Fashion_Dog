9b14e9d309161a98ddaea73e55a0cddd91169d7f
Drago286 <drago.peric@alumnos.ucn.cl>
2022-06-30 20:41:02 -0400

    confirmar

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -71,37 +71,12 @@ class SolicitudController extends Controller
         //
     }
 
-    public function agregarComentario(Request $request, $id)
-    {
-
-        $solicitud = Solicitud::where('id', $id)->FirstOrFail();
-
-        $solicitud->comentario = $request->comentario;
-
-        $solicitud->save();
-
-        return redirect('/cliente');
-    }
-
-
-
-    public function cancelStatusSolicitud($request)
-    {
-        $solicitud = Solicitud::where('id', $request)->get()->first();
-            $solicitud->estado = 'ANULADA';
-            $solicitud->save();
-            session()->flash('anular', 'La Solicitud fue anulada con exito!');
-            return redirect('/cliente');
-    }
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -158,54 +133,4 @@ class SolicitudController extends Controller
     {
         //
     }
-
-
-    public function store(Request $request)
-    {
-
-        $date = date($request->fecha_solicitud);
-        $time = date($request->hora_solicitud);
-        $solicituds = Auth::user()->solicitudesCliente()->get();
-
-        // $request->validate([
-        //     'fecha_solicitud' => ['required', 'date', 'regex:'],
-        // ]);
-
-        switch ($date) {
-            case null:
-                throw ValidationException::withMessages(['fecha_solicitud' => 'Debe seleccionar una fecha.']);
-                break;
-
-            case ($date < date("Y-m-d")):
-                throw ValidationException::withMessages(['fecha_solicitud' => 'Las solicitudes se pueden realizar desde: ' . date("d-m-Y")]);
-                break;
-
-            case ($date >= "9999-12-31"):
-                throw ValidationException::withMessages(['fecha_solicitud' => 'La fecha indicada no es válida, debe seguir el formato: DD/MM/YYYY.']);
-                break;
-        }
-        if ($time == null) {
-            throw ValidationException::withMessages(['hora_solicitud' => 'Debe seleccionar una hora.']);
-        }
-
-
-        foreach ($solicituds as $solicitud) {
-
-            if ($solicitud->fecha_solicitud == $date && $solicitud->estado == "INGRESADA") {
-                throw ValidationException::withMessages(['fecha_solicitud' => 'Ya existe solicitud para la fecha:' . $date]);
-            }
-        }
-
-
-        Solicitud::create([
-            'fecha_solicitud' => $date,
-            'hora_solicitud' => $time,
-            'estado' => "INGRESADA",
-            'cliente_id' => Auth::user()->id,
-        ]);
-
-        session()->flash('exito', 'El servicio fue solicitado con exito!');
-        return redirect(route('home'));
-    }
-
 }
