0ce4fa1ecceca9fcabd31fea392f11cdf1bd7bcc
diegoAguilera11 <diego.aguilera@alumnos.ucn.cl>
2022-07-21 20:07:18 -0400

    Ultimo cambio registro

--- resources/views/administrador/create.blade.php
+++ resources/views/administrador/create.blade.php
@@ -36,7 +36,7 @@
 
                                 <div class="col-md-6">
                                     <input id="nombre" type="text"
-                                        oninput="this.value = this.value.replace(/[^A-Z\\a-z]/g, '').replace(/(\..*)\./g, '$1');"
+                                        oninput="this.value = this.value.replace(/[^A-Z\\a-z\\a-zA-ZñÑáéíóúÁÉÍÓÚ\s\\]/g, '').replace(/(\..*)\./g, '$1');"
                                         class="form-control @error('nombre') is-invalid @enderror" name="nombre"
                                         value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
 
@@ -56,7 +56,7 @@
 
                                 <div class="col-md-6">
                                     <input id="apellidoPaterno" type="text"
-                                        oninput="this.value = this.value.replace(/[^A-Z\\a-z]/g, '').replace(/(\..*)\./g, '$1');"
+                                        oninput="this.value = this.value.replace(/[^A-Z\\a-z\\a-zA-ZñÑáéíóúÁÉÍÓÚ\s\\]/g, '').replace(/(\..*)\./g, '$1');"
                                         class="form-control @error('apellidoPaterno') is-invalid @enderror"
                                         name="apellidoPaterno" value="{{ old('apellidoPaterno') }}" required
                                         autocomplete="apellidoPaterno" autofocus>
